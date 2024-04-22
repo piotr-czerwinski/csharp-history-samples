13b21e58f33eec7b0d36773edafbc952053888ab
Piotr Czerwiński <piotr.czerwinski.dev@gmail.com>
2024-04-22 21:33:14 +0200

    C# 11 SetsRequiredMembers added for Required members

--- CSharpHistorySamples/V11/V11_RequiredMembers.cs
+++ CSharpHistorySamples/V11/V11_RequiredMembers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Diagnostics.CodeAnalysis;
 
 namespace CSharpHistorySamples;
 
@@ -8,7 +9,7 @@ internal static partial class V11
     internal static void RequiredMembers()
     {
         /* 
-         * Would not compile with en error
+         * Would not compile. Error:
          * Required member 'V11.Person.Last' must be set in the object initializer or attribute constructor.
         var person = new Person()
         {
@@ -18,9 +19,11 @@ internal static partial class V11
 
         var person = new Person()
         {
-            First = "First name",
-            Last = "Last name"
+            FirstName = "First name",
+            LastName = "Last name"
         };
+
+        _ = new SimplePerson(); // No error, as constructor marked with [SetsRequiredMembers]
     }
 
     // Property must be set as error states:
@@ -31,9 +34,11 @@ internal static partial class V11
     {
         // When not marked as required, it would be possible to construct object with no value set
         // as there is no constructor setting any values
-        public required string First { get; init;}
-        public string? Middle { get; init;}
-        public required string Last { get; init;}
+        public required string FirstName { get; init; }
+
+        public string? MiddleName { get; init; }
+
+        public required string LastName { get; init; }
     }
 
     private class SampleAtrAttribute : Attribute
@@ -42,5 +47,21 @@ internal static partial class V11
         {
             get; init;
         }
+    }
+
+    private class SimplePerson
+    {
+        [SetsRequiredMembers]
+        public SimplePerson()
+        {
+            // warning would be raised raised if values
+            // had not been assigned for required properties
+            FirstName = "";
+            LastName = "";
+        }
+
+        public required string FirstName { get; init; }
+
+        public required string LastName { get; init; }
     }
 }
